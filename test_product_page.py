a7286ff197e40553f09cf9c91b7f8fbc41531922
Тиганов Денис Юрьевич <Denis.Tiganov@norbit.ru>
2023-11-25 14:11:03 +0300

    Test on fail added.

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,9 @@
 from .pages.product_page import ProductPage
 import pytest
-#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+from .pages.locators import ProductPageLocators
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
 
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -18,3 +20,35 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.add_product_to_basket()
     page.should_be_in_basket()
     page.should_be_same_price()
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    assert page.is_not_element_present(*ProductPageLocators.ADDED_PRODUCT_NAME), "Message present after product added to busket, but should not be"
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    assert page.is_not_element_present(*ProductPageLocators.ADDED_PRODUCT_NAME), "Message present without adding product to basket, but should not be"
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    assert page.is_disappeared(*ProductPageLocators.ADDED_PRODUCT_NAME), "Message did not dissapear after adding product to basket"
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
